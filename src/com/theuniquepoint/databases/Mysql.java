c8dee969d249bb3401b2749075e680d81800bfa1
Alex <yum-y@DESKTOP-3HGDSO5>
2017-03-10 22:47:05 +0100

    Databases - MySQL class development.

--- src/com/theuniquepoint/databases/Mysql.java
+++ src/com/theuniquepoint/databases/Mysql.java
@@ -17,34 +17,62 @@ import java.util.Map;
 /**
  *
  * <h1>Databases - Mysql</h1>
- * The Mysql class implements 
- * 
+ * The Mysql class implements methods to manage databases more easily.
+ *
  * @author Alex "yum-y" Torregrosa Romero
  * @version 1.00
  * @since 2017-03-01
  */
 public class Mysql {
+
     private void exception_error(Exception e) {
         System.out.println(e);
         System.exit(0);
     }
+
+    /**
+     * Opens a connection to the MySQL Server running on.
+     *
+     * @param Host Can be either a host name or an IP address.
+     * @param User The MySQL user name.
+     * @param Pass If not provided or NULL, the MySQL server will attempt to
+     * authenticate the user against those user records which have no password
+     * only.
+     * @param Database The default database to be used when performing queries.
+     * @return Returns an object which represents the connection to a MySQL
+     * Server.
+     */
     public Connection connect(String Host, String User, String Pass, String Database) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://"+Host+":3306/"+Database, User, Pass);  
-        } catch(ClassNotFoundException | SQLException e) {
+            return DriverManager.getConnection("jdbc:mysql://" + Host + ":3306/" + Database, User, Pass);
+        } catch (ClassNotFoundException | SQLException e) {
             exception_error(e);
         }
         return null;
     }
+
+    /**
+     * Performs an execute query on the database.
+     * 
+     * @param link A link identifier returned by connect().
+     * @param q The query string.
+     */
     public void execute(Connection link, String q) {
         try {
             Statement st = link.createStatement();
             st.executeUpdate(q);
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             exception_error(e);
         }
     }
+
+    /**
+     * Fetch a result row as an associative map.
+     * 
+     * @param result A result set identifier returned by query().
+     * @return Returns an associative map that corresponds to the fetched row or NULL if there are no more rows.
+     */
     public Map<String, String> fetch_assoc(ResultSet result) {
         try {
             if (result.next()) {
@@ -60,6 +88,13 @@ public class Mysql {
         }
         return null;
     }
+
+    /**
+     * Get a result row as an enumerated array.
+     * 
+     * @param result A result set identifier returned by query().
+     * @return Returns an array of strings that corresponds to the fetched row or NULL if there are no more rows.
+     */
     public String[] fetch_row(ResultSet result) {
         try {
             if (result.next()) {
@@ -74,6 +109,13 @@ public class Mysql {
         }
         return null;
     }
+
+    /**
+     * Returns the number of columns in statements result set.
+     * 
+     * @param result A result set identifier returned by query().
+     * @return An integer representing the number of fields in a result set.
+     */
     public int field_count(ResultSet result) {
         try {
             ResultSetMetaData resultmd = result.getMetaData();
@@ -83,6 +125,13 @@ public class Mysql {
         }
         return 0;
     }
+
+    /**
+     * Return the number of rows in statements result set.
+     * 
+     * @param result A result set identifier returned by query().
+     * @return An integer representing the number of rows in result set.
+     */
     public int num_rows(ResultSet result) {
         int rows;
         try {
@@ -95,15 +144,29 @@ public class Mysql {
         }
         return 0;
     }
+
+    /**
+     * Performs a result query on the database.
+     * 
+     * @param link A link identifier returned by connect().
+     * @param q The query string.
+     * @return Returns NULL on failure. For successful SELECT, SHOW, DESCRIBE or EXPLAIN queries query() will return a ResultSet object.
+     */
     public ResultSet query(Connection link, String q) {
         try {
             Statement st = link.createStatement();
             return st.executeQuery(q);
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             exception_error(e);
         }
         return null;
     }
+
+    /**
+     * Closes a previously opened database connection.
+     * 
+     * @param link A link identifier returned by connect().
+     */
     public void close(Connection link) {
         try {
             link.close();
