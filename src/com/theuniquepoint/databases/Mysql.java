ceb92c8c61508c0f8d1acf4b270725f2515c51d6
Alex <yum-y@DESKTOP-3HGDSO5>
2017-03-11 00:47:02 +0100

    Databases - MySQL class development.

--- src/com/theuniquepoint/databases/Mysql.java
+++ src/com/theuniquepoint/databases/Mysql.java
@@ -25,7 +25,7 @@ import java.util.Map;
  */
 public class Mysql {
 
-    private void exception_error(Exception e) {
+    private void exception(Exception e) {
         System.out.println(e);
         System.exit(0);
     }
@@ -47,92 +47,122 @@ public class Mysql {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection("jdbc:mysql://" + Host + ":3306/" + Database, User, Pass);
         } catch (ClassNotFoundException | SQLException e) {
-            exception_error(e);
+            exception(e);
         }
         return null;
     }
 
     /**
-     * Performs an execute query on the database.
+     * Frees the memory associated with a result
+     *
+     * @param result A result set identifier returned by queryRead().
+     */
+    public void freeResult(ResultSet result) {
+        try {
+            result.close();
+        } catch (SQLException e) {
+            exception(e);
+        }
+    }
+    
+    /**
+     * Returns a string representing the type of connection used.
      * 
      * @param link A link identifier returned by connect().
-     * @param q The query string.
+     * @return A character string representing the server hostname and the connection type.
      */
-    public void execute(Connection link, String q) {
+    public String getHostInfo(Connection link) {
         try {
-            Statement st = link.createStatement();
-            st.executeUpdate(q);
+            return link.getMetaData().getDatabaseProductName();
         } catch (SQLException e) {
-            exception_error(e);
+            exception(e);
         }
+        return null;
+    }
+    
+    /**
+     * Returns the version of the MySQL server.
+     * 
+     * @param link A link identifier returned by connect().
+     * @return A character string representing the server version.
+     */
+    public String getServerInfo(Connection link) {
+        try {
+            return link.getMetaData().getDatabaseProductVersion();
+        } catch (SQLException e) {
+            exception(e);
+        }
+        return null;
     }
 
     /**
      * Fetch a result row as an associative map.
-     * 
-     * @param result A result set identifier returned by query().
-     * @return Returns an associative map that corresponds to the fetched row or NULL if there are no more rows.
+     *
+     * @param result A result set identifier returned by queryRead().
+     * @return Returns an associative map that corresponds to the fetched row or
+     * NULL if there are no more rows.
      */
-    public Map<String, String> fetch_assoc(ResultSet result) {
+    public Map<String, String> fetchAssoc(ResultSet result) {
         try {
             if (result.next()) {
                 Map<String, String> values = new HashMap<>();
                 ResultSetMetaData resultmd = result.getMetaData();
-                for (int i = 0; i < field_count(result); i++) {
+                for (int i = 0; i < fieldCount(result); i++) {
                     values.put(resultmd.getColumnName(i + 1), result.getString(i + 1));
                 }
                 return values;
             }
         } catch (SQLException e) {
-            exception_error(e);
+            exception(e);
         }
         return null;
     }
 
     /**
      * Get a result row as an enumerated array.
-     * 
-     * @param result A result set identifier returned by query().
-     * @return Returns an array of strings that corresponds to the fetched row or NULL if there are no more rows.
+     *
+     * @param result A result set identifier returned by queryRead().
+     * @return Returns an array of strings that corresponds to the fetched row
+     * or NULL if there are no more rows.
      */
-    public String[] fetch_row(ResultSet result) {
+    public String[] fetchRow(ResultSet result) {
         try {
             if (result.next()) {
-                String values[] = new String[field_count(result)];
-                for (int i = 0; i < field_count(result); i++) {
+                String values[] = new String[fieldCount(result)];
+                for (int i = 0; i < fieldCount(result); i++) {
                     values[i] = result.getString(i + 1);
                 }
                 return values;
             }
         } catch (SQLException e) {
-            exception_error(e);
+            exception(e);
         }
         return null;
     }
 
     /**
      * Returns the number of columns in statements result set.
-     * 
-     * @param result A result set identifier returned by query().
+     *
+     * @param result A result set identifier returned by queryRead().
      * @return An integer representing the number of fields in a result set.
      */
-    public int field_count(ResultSet result) {
+    public int fieldCount(ResultSet result) {
         try {
             ResultSetMetaData resultmd = result.getMetaData();
             return resultmd.getColumnCount();
         } catch (SQLException e) {
-            exception_error(e);
+            exception(e);
         }
         return 0;
     }
 
     /**
      * Return the number of rows in statements result set.
-     * 
-     * @param result A result set identifier returned by query().
+     *
+     * @param result A result set identifier returned by queryRead().
      * @return An integer representing the number of rows in result set.
      */
-    public int num_rows(ResultSet result) {
+    public int numRows(ResultSet result) {
         int rows;
         try {
             result.last();
@@ -140,38 +170,54 @@ public class Mysql {
             result.beforeFirst();
             return rows;
         } catch (SQLException e) {
-            exception_error(e);
+            exception(e);
         }
         return 0;
     }
 
     /**
-     * Performs a result query on the database.
-     * 
+     * Performs an execute query on the database.
+     *
      * @param link A link identifier returned by connect().
      * @param q The query string.
-     * @return Returns NULL on failure. For successful SELECT, SHOW, DESCRIBE or EXPLAIN queries query() will return a ResultSet object.
      */
-    public ResultSet query(Connection link, String q) {
+    public void queryExecute(Connection link, String q) {
+        try {
+            Statement st = link.createStatement();
+            st.executeUpdate(q);
+        } catch (SQLException e) {
+            exception(e);
+        }
+    }
+    
+    /**
+     * Performs a result query on the database.
+     *
+     * @param link A link identifier returned by connect().
+     * @param q The query string.
+     * @return Returns NULL on failure. For successful SELECT, SHOW, DESCRIBE or
+     * EXPLAIN queries queryRead() will return a ResultSet object.
+     */
+    public ResultSet queryRead(Connection link, String q) {
         try {
             Statement st = link.createStatement();
             return st.executeQuery(q);
         } catch (SQLException e) {
-            exception_error(e);
+            exception(e);
         }
         return null;
     }
-
+    
     /**
      * Closes a previously opened database connection.
-     * 
+     *
      * @param link A link identifier returned by connect().
      */
     public void close(Connection link) {
         try {
             link.close();
         } catch (SQLException e) {
-            exception_error(e);
+            exception(e);
         }
     }
 }
