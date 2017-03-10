d5adbef7ba6e37213e8dc3dd3b222bf72f7e8624
Alex <yum-y@DESKTOP-3HGDSO5>
2017-03-10 21:15:38 +0100

    Databases - MySQL class development.

--- src/com/theuniquepoint/databases/Mysql.java
+++ src/com/theuniquepoint/databases/Mysql.java
@@ -8,8 +8,11 @@ package com.theuniquepoint.databases;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -21,42 +24,91 @@ import java.sql.Statement;
  * @since 2017-03-01
  */
 public class Mysql {
-    private Exception errno, connect_errno;
-    private String error, connect_error;
-    
+    private void exception_error(Exception e) {
+        System.out.println(e);
+        System.exit(0);
+    }
     public Connection connect(String Host, String User, String Pass, String Database) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection("jdbc:mysql://"+Host+":3306/"+Database, User, Pass);  
         } catch(ClassNotFoundException | SQLException e) {
-            this.connect_errno = e;
-            this.connect_error = "Cannot connect to the server.";
+            exception_error(e);
         }
         return null;
     }
-    public Exception connect_errno() {
-        return connect_errno;
+    public void execute(Connection link, String q) {
+        try {
+            Statement st = link.createStatement();
+            st.executeUpdate(q);
+        } catch(SQLException e) {
+            exception_error(e);
+        }
     }
-    public String connect_error() {
-        return connect_error;
-    }
-    public Exception errno() {
-        return errno;
-    }
-    public String error() {
-        return error;
-    }
-    public String[] fetch_array(ResultSet result, String fetch) {
+    public Map<String, String> fetch_assoc(ResultSet result) {
+        try {
+            if (result.next()) {
+                Map<String, String> values = new HashMap<>();
+                ResultSetMetaData resultmd = result.getMetaData();
+                for (int i = 0; i < field_count(result); i++) {
+                    values.put(resultmd.getColumnName(i + 1), result.getString(i + 1));
+                }
+                return values;
+            }
+        } catch (SQLException e) {
+            exception_error(e);
+        }
         return null;
+    }
+    public String[] fetch_row(ResultSet result) {
+        try {
+            if (result.next()) {
+                String values[] = new String[field_count(result)];
+                for (int i = 0; i < field_count(result); i++) {
+                    values[i] = result.getString(i + 1);
+                }
+                return values;
+            }
+        } catch (SQLException e) {
+            exception_error(e);
+        }
+        return null;
+    }
+    public int field_count(ResultSet result) {
+        try {
+            ResultSetMetaData resultmd = result.getMetaData();
+            return resultmd.getColumnCount();
+        } catch (SQLException e) {
+            exception_error(e);
+        }
+        return 0;
+    }
+    public int num_rows(ResultSet result) {
+        int rows;
+        try {
+            result.last();
+            rows = result.getRow();
+            result.beforeFirst();
+            return rows;
+        } catch (SQLException e) {
+            exception_error(e);
+        }
+        return 0;
     }
     public ResultSet query(Connection link, String q) {
         try {
             Statement st = link.createStatement();
             return st.executeQuery(q);
         } catch(SQLException e) {
-            this.errno = e;
-            this.error = "Cannot execute the query.";
+            exception_error(e);
         }
         return null;
+    }
+    public void close(Connection link) {
+        try {
+            link.close();
+        } catch (SQLException e) {
+            exception_error(e);
+        }
     }
 }
---
+++ test/com/theuniquepoint/databases/Demo.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.theuniquepoint.databases;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.Map;
+
+/**
+ *
+ * @author mati
+ */
+public class Demo {
+    static Mysql mysql = new Mysql();
+    
+    public static void main(String[] args) {
+        printRow();
+    }
+    static void exampleDatabase() {
+        Connection link = mysql.connect("sql11.freemysqlhosting.net", "sql11163128", "VJZCP1VGTl", "sql11163128");
+        
+        mysql.execute(link, "CREATE TABLE Servers ( ID int NOT NULL AUTO_INCREMENT, Name varchar(255), WebService ENUM('Yes', 'No'), DNService ENUM('Yes', 'No'), FileService ENUM('Yes', 'No'), DBService ENUM('Yes', 'No'), MailService ENUM('Yes', 'No'), VServer ENUM('Yes', 'No'), PRIMARY KEY (ID) )");
+        mysql.execute(link, "INSERT INTO Servers(Name, WebService, DNService, FileService, DBService, MailService, VServer) VALUES ('ws01.theuniquepoint.com', 'Yes', 'No', 'Yes', 'No', 'No', 'No')");
+        mysql.execute(link, "INSERT INTO Servers(Name, WebService, DNService, FileService, DBService, MailService, VServer) VALUES ('ns01.theuniquepoint.com', 'No', 'Yes', 'No', 'No', 'No', 'No')");
+        mysql.execute(link, "INSERT INTO Servers(Name, WebService, DNService, FileService, DBService, MailService, VServer) VALUES ('ns02.theuniquepoint.com', 'No', 'Yes', 'No', 'No', 'No', 'No')");
+        mysql.execute(link, "INSERT INTO Servers(Name, WebService, DNService, FileService, DBService, MailService, VServer) VALUES ('mysql01.theuniquepoint.com', 'No', 'No', 'No', 'Yes', 'No', 'No')");
+        mysql.execute(link, "INSERT INTO Servers(Name, WebService, DNService, FileService, DBService, MailService, VServer) VALUES ('mysql02.theuniquepoint.com', 'No', 'No', 'No', 'Yes', 'No', 'No')");
+        mysql.execute(link, "INSERT INTO Servers(Name, WebService, DNService, FileService, DBService, MailService, VServer) VALUES ('mail01.theuniquepoint.com', 'No', 'No', 'No', 'No', 'Yes', 'No')");
+        mysql.execute(link, "INSERT INTO Servers(Name, WebService, DNService, FileService, DBService, MailService, VServer) VALUES ('vm01.theuniquepoint.com', 'No', 'No', 'No', 'No', 'No', 'Yes')");
+        mysql.execute(link, "INSERT INTO Servers(Name, WebService, DNService, FileService, DBService, MailService, VServer) VALUES ('vm02.theuniquepoint.com', 'No', 'No', 'No', 'No', 'No', 'Yes')");
+        
+        mysql.close(link);
+    }
+    static void printRow() {
+        String row[];
+        
+        Connection link = mysql.connect("sql11.freemysqlhosting.net", "sql11163128", "VJZCP1VGTl", "sql11163128");
+        
+        ResultSet result = mysql.query(link, "SELECT * FROM Servers WHERE DNService = 'Yes'");
+        System.out.println("Retrieving data form database, number of rows: " + mysql.num_rows(result) + "\n");
+        
+        System.out.println("ID, Name, WebService, DNService, FileService, DBService, MailService, VServer");
+        while ((row = mysql.fetch_row(result)) != null) {
+            for (int i = 0; i < mysql.field_count(result); i++) {
+                System.out.print(row[i] + " ");
+            }
+            System.out.println("");
+        }
+        
+        mysql.close(link);
+    }
+    static void printAssoc() {
+        Map<String, String> row;
+        
+        Connection link = mysql.connect("sql11.freemysqlhosting.net", "sql11163128", "VJZCP1VGTl", "sql11163128");
+        
+        ResultSet result = mysql.query(link, "SELECT * FROM Servers");
+        System.out.println("Retrieving data form database, number of rows: " + mysql.num_rows(result) + "\n");
+        
+        System.out.println("ID, Name, WebService, DNService, FileService, DBService, MailService, VServer");
+        while ((row = mysql.fetch_assoc(result)) != null) {
+            System.out.println(row.get("ID") + " " + row.get("Name") + " " + row.get("WebService") + " " + row.get("DNService") + " " + row.get("FileService") + " " + row.get("DBService") + " " + row.get("MailService") + " " + row.get("VServer"));
+        }
+        
+        mysql.close(link);
+    }
+}
